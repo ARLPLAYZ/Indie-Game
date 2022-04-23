3d5d3a893e276ba29f541ac3a30fc2899e900cc1
ARLPLAYZ <98569491+ARLPLAYZ@users.noreply.github.com>
2022-04-23 13:07:43 +0530

    Add files via upload

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,95 @@
 function createPlayer() {
     const player = createSprite(windowWidth/2-800, windowHeight/2 + 200, 50, 50);
-    player.addImage(leftI);
     player.scale = 0.2;
     return player;
 }
 function controls(player) {
-    if (keyDown(37)) player.addImage(leftI)
-    else if (keyDown(39)) player.addImage(rightI)
-    else if (keyDown(38)) player.addImage(topI)
-    else if (keyDown(40)) player.addImage(bottomI);
+    if (keyDown(37)) player.x = player.x -3;
+    if (keyDown(38)) player.y = player.y -3;
+    if (keyDown(39)) player.x = player.x + 3;
+    if (keyDown(40)) player.y = player.y + 3;
+    if (keyDown('space')) shootBullet();
 }
+function generateObstacle() {
+    const obstacle = createSprite(random(windowWidth), random(windowHeight), random(100), random(300));
+    return obstacle;
+}
+
+function checkIfOutsideBorder() {
+
+    if (player.x <= 1) player.x = player.x + 20;
+    if (player.x > windowWidth/2 + 500) player.x = player.x - 20;
+    
+    if (player.y <= 1) player.y = player.y + 20;
+    if (player.y > windowHeight/2 + 270) player.y = player.y - 20;
+
+}
+function bulletCheck(sprite) {
+
+    if (sprite.x <= 1) sprite.destroy();
+    if (sprite.x > windowWidth/2 + 500) sprite.destroy();
+    
+    if (sprite.x <= 1) sprite.destroy();
+    if (sprite.x > windowWidth/2 + 500) sprite.destroy();
+}
+
+function shootBullet() {
+    if (Date.now() < coolDown) return;
+    bullet = createSprite(150, windowWidth/2, 50, 20);
+    bullet.addAnimation('e', wpnAnim);
+    let facing = player.getAnimationLabel();    
+    switch(facing) {
+        case "r": bullet.velocityX = 5;
+        bullet.x = player.x + 50;
+        bullet.y = player.y;
+        break;
+        case "l": bullet.velocityX = -5;
+        bullet.x = player.x -50;
+        bullet.y = player.y;
+        break;
+        case "b": bullet.velocityY = 5;
+        bullet.x = player.x;
+        bullet.y = player.y + 50;
+        break;
+        case "t": bullet.velocityY = -5;
+        bullet.x = player.x;
+        bullet.y = player.y - 50;
+        break;        
+    }
+    bullet.scale = 0.1;
+    bullet.lifetime = 150;
+
+
+    
+    coolDown = Date.now() + 1e3/2;
+    
+    
+}
+
+function randomTrueFalse() {
+    return [true,false][Math.round(Math.random(([true, false].length -1)))]
+}
+function sleep(ms) {
+    return new Promise(res=> setTimeout(res(), ms))
+}
+async function enemySpawn() {
+
+    while(enemies.length < 5) {
+        const enemy = createSprite(random(windowWidth/2), random(windowHeight/2), 20, 20);
+        enemy.addImage('b',loadImage('images/ugly.png'))
+        enemy.scale = 0.07;
+        enemy.friction = 0.5;
+        enemy.attractionPoint(2, player.position.x, player.position.y)
+        enemies.push(enemy);
+
+        
+    }
+    // const enemy = createSprite(random(windowWidth/2), random(windowHeight/2), 20, 20);
+   /* let velocityY;
+    velocityX ? velocityY = 0 : velocityY = 1;
+    enemy.setVelocity(velocityX, velocityY);*/
+       // return enemy;
+}
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,25 +1,90 @@
 
-let bgImg, leftI, rightI, topI, bottomI;
+let bgImg, leftI, rightI, topI, bottomI, wpn1Img, wpn2Img;
+let player, mountain;
+let bullet;
+let ob, weapon, wpnAnim;
+let lvl1;
+let enemies = [];
+let bullets = [];
+let coolDown = Date.now();
 function preload() {
-    bgImg = loadImage('./images/2.png');
-    m1Img = loadImage('./images/3-removebg-preview.png');
-    m2Img = loadImage('./images/4-removebg-preview.png');    
+    bgImg = loadImage('./images/lv2.png');
     leftI = loadImage('./images/left.png');
     rightI = loadImage('./images/right.png');
     topI = loadImage('./images/top.png');
     bottomI = loadImage('./images/bottom.png');
+    wpnAnim = loadAnimation('images/shiru2.png', 'images/shiru3.png');
+    lvl1 = loadImage('images/lvl1.png');
+    wpnAnim.frameDelay = 2;
 
 
 }
 function setup() {
     createCanvas(windowWidth-300, windowHeight-100);
     
+    player = createPlayer();
+    player.addImage('r', rightI);
+    player.addImage('l', leftI);
+    player.addImage('t', topI);
+    player.addImage('b', bottomI);
+    player.debug = true;
+    mountain = createSprite(windowWidth / 2 , windowHeight -10,10,10);
+   
 
-    const mountain = createSprite(windowWidth / 2 , windowHeight -10,10,10)
+    
+
+
 }
 function draw() {
-    background('#42cef5');
-    let mainGuy = createPlayer();
-    controls(mainGuy);
+    background(lvl1);  
+    controls(player);
+    checkIfOutsideBorder(); 
+    enemySpawn();
+
+        for (const enemy of enemies) {
+            
+            enemy.friction = 0.5;
+            enemy.attractionPoint(1.5, player.position.x, player.position.y);
+            if (bullet) continue;
+            if (enemy.isTouching(player)) {
+                enemy.destroy();
+                player.destroy();
+            }           
+        }
+        for (const e of enemies) {
+            const eGroup = new Group();
+            const bGroup = new Group();
+            eGroup.add(e);
+            
+            for (const b of bullets) {
+                bGroup.add(b);
+                if (bGroup.collide(eGroup)) {
+                    bGroup.destroyEach();
+                }
+            }
+        }
+    /*
+    e.friction = 0.5;
+    e.attractionPoint(2, player.position.x, player.position.y);
+    */
+   
     drawSprites();
 }
+function keyPressed() {
+    if (keyCode === 37) {
+        player.changeImage('l');
+
+    }
+    if (keyCode === 39) {
+        player.changeImage('r'); 
+        
+    }  
+    if (keyCode === 38) {
+        player.changeImage('t');
+
+    }
+    if (keyCode === 40) {
+        player.changeImage('b');
+    }
+}
+
